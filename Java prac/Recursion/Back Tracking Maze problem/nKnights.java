8bd3ba84a8c212b6c2127126e2f3be2dfcff71a0
BhavyaSinghal07 <bhavya.singhal.cseai.2022@miet.ac.in>
2025-06-26 11:54:36 +0530

    knight

--- Java prac/Recursion/Back Tracking Maze problem/nKnights.java
+++ Java prac/Recursion/Back Tracking Maze problem/nKnights.java
@@ -3,6 +3,9 @@
 public class nKnights {
     public static void main(String[] args) {
         
+        int n = 4;
+        boolean[][] board = new boolean[n][n];
+        knight(board, 0, 0, 4);
     }
 
 
@@ -11,9 +14,10 @@ public class nKnights {
         if (knights == 0) {
             display(board);
             System.out.println();
+            return;
         }
         
-        if(col == board.length){
+        if (col == board.length){
             knight(board, row + 1, 0, knights);
             return;
         }
@@ -29,8 +33,39 @@ public class nKnights {
     }
 
     private static boolean isSafe(boolean[][] board, int row, int col){
-        
+        if (isValid(board, row - 1, col - 2)){
+            if(board[row - 1][col - 2]){
+                return false;
+            }
+        }
+
+        if (isValid(board, row - 2, col - 1)){
+            if(board[row - 2][col - 1]){
+                return false;
+            }
+        }
+
+        if (isValid(board, row - 2, col + 1)){
+            if(board[row - 2][col + 1]){
+                return false;
+            }
+        }
+
+        if (isValid(board, row - 1, col + 2)){
+            if(board[row - 1][col +2]){
+                return false;
+            }
+        }
+
         return true;
+    }
+
+    // do not repeat yourself, hence created this function
+    static boolean isValid(boolean[][] board, int row, int col){
+        if(row >= 0 && row < board.length && col >= 0 && col < board.length){
+            return true;
+        }
+        return false;
     }
 
 
